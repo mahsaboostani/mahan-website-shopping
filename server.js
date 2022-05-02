a3fc84602ae6431e72220829c416f86a4ff5089f
mahsaboostani <eng.m.boostani@gmail.com>
2022-05-02 10:37:18 +0200

    hosting a json-server

--- server.js
+++ server.js
@@ -5,16 +5,3 @@ const app = express();
 app.use(serveStatic(path.join(__dirname, "dist")));
 const port = process.env.PORT || 80;
 app.listen(port);
-
-const jsonServer = require("json-server");
-const server = jsonServer.create();
-const router = jsonServer.router("db.json");
-
-const middlewares = jsonServer.defaults();
-
-server.use(middlewares);
-server.use(router);
-
-const ports = process.env.PORT || 3000;
-
-server.listen(ports);
