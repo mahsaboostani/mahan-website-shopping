607dc0e024ff7ea24f3696df9445993a816cf887
mahsaboostani <eng.m.boostani@gmail.com>
2022-05-02 10:05:26 +0200

    hosting a json-server

--- server.js
+++ server.js
@@ -5,3 +5,19 @@ const app = express();
 app.use(serveStatic(path.join(__dirname, "dist")));
 const port = process.env.PORT || 80;
 app.listen(port);
+
+
+const jsonServer = require('json-server')
+const server = jsonServer.create()
+const router = jsonServer.router('db.json')
+
+const middlewares = jsonServer.defaults()
+
+server.use(middlewares)
+server.use(router)
+
+port = process.env.PORT || 3000
+
+server.listen(port, () => {
+    console.log(`JSON Server is running on port ${port}`)
+})
