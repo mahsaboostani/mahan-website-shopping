12a98d7573b6a10081d0cabdeeee3dc27f5c04ee
mahsaboostani <eng.m.boostani@gmail.com>
2022-05-02 12:09:32 +0200

    hosting a json-server

--- server.js
+++ server.js
@@ -5,16 +5,3 @@ const app = express();
 app.use(serveStatic(path.join(__dirname, "dist")));
 const port = process.env.PORT || 80;
 app.listen(port);
-
-// const jsonServer = require("json-server");
-// const server = jsonServer.create();
-// const router = jsonServer.router(
-//   "https://raw.githubusercontent.com/mahsaboostani/mahan-website-shopping/main/db.json"
-// );
-// const middlewares = jsonServer.defaults();
-
-// server.use(middlewares);
-// server.use(router);
-// server.listen(3000, () => {
-//   console.log("JSON Server is running");
-// });
